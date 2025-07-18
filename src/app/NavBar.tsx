8a3560da433711c1bc5b9b52317fa07ee8bb9fe8
James Winfield <hello@jameswinfield.co.uk>
2025-07-18 20:47:15 +0100

    remove nextauth

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -2,10 +2,8 @@
 
 import Link from "next/link";
 import styles from "./NavBar.module.css";
-import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function NavBar() {
-  const { data: session, status } = useSession();
   return (
     <nav className={styles.nav}>
       <ul>
@@ -14,23 +12,6 @@ export default function NavBar() {
         </li>
         <li>
           <Link href="/pubs">All Pubs</Link>
-        </li>
-        {/* Add more links as needed */}
-        <li style={{ marginLeft: "auto" }}>
-          {status === "loading" ? (
-            <span>Loading…</span>
-          ) : session ? (
-            <>
-              <span>
-                Signed in as {session.user?.name || session.user?.email}
-              </span>
-              <button onClick={() => signOut()} style={{ marginLeft: 8 }}>
-                Sign out
-              </button>
-            </>
-          ) : (
-            <button onClick={() => signIn()}>Sign in</button>
-          )}
         </li>
       </ul>
     </nav>
