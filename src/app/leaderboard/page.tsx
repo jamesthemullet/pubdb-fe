33d105655032407cde7eb145dd69dc29fb4ea176
James Winfield <hello@jameswinfield.co.uk>
2026-06-04 19:32:47 +0100

    format

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useAuth } from "@/hooks/useAuth";
 import { type LeaderboardEntry, useLeaderboard } from "@/hooks/useLeaderboard";
 import styles from "./page.module.css";
